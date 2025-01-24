b904c1b66e6a91f7359b1f242545857cfac3c6bd
esteranodin <esteranodin@hotmail.fr>
2025-01-24 16:43:16 +0100

    MAJ: BDD

--- utils/database/bookmaker_td.sql
+++ utils/database/bookmaker_td.sql
@@ -1,86 +1,59 @@
-CREATE TABLE `note`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `id_user` BIGINT NOT NULL,
-    `id_book` BIGINT NOT NULL,
-    `score` BIGINT NOT NULL
-);
-CREATE TABLE `categorie`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `name` BIGINT NOT NULL
-);
-CREATE TABLE `book_categorie`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `id_categorie` BIGINT NOT NULL,
-    `id_book` BIGINT NOT NULL
-);
-CREATE TABLE `images`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `img_path` VARCHAR(255) NOT NULL,
-    `img_alt` VARCHAR(255) NOT NULL
-);
-CREATE TABLE `author`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `lastname` VARCHAR(255) NOT NULL,
-    `firstname` VARCHAR(255) NOT NULL
-);
-CREATE TABLE `book`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `id_saler` BIGINT NOT NULL,
-    `isbn` VARCHAR(255) NOT NULL,
-    `title` VARCHAR(255) NOT NULL,
-    `id_author` BIGINT NOT NULL,
-    `parutionAt` DATE NOT NULL,
-    `edition` VARCHAR(255) NOT NULL,
-    `description` TEXT NOT NULL,
-    `price` BIGINT NOT NULL,
-    `id_image` BIGINT NOT NULL
-);
-CREATE TABLE `sales`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `id_isbn` BIGINT NOT NULL,
-    `purchaseAt` DATETIME NOT NULL,
-    `id_saler` BIGINT NOT NULL,
-    `id_buyer` BIGINT NOT NULL
-);
-ALTER TABLE
-    `sales` ADD UNIQUE `sales_id_isbn_unique`(`id_isbn`);
-CREATE TABLE `user`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `mail` VARCHAR(191) NOT NULL,
-    `password` VARCHAR(255) NOT NULL,
-    `lastname` VARCHAR(255) NOT NULL,
-    `firstname` VARCHAR(255) NOT NULL,
-    `role` VARCHAR(255) NOT NULL
-);
-ALTER TABLE
-    `user` ADD UNIQUE `user_mail_unique`(`mail`);
-CREATE TABLE `user_pro`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `phone` VARCHAR(255) NOT NULL,
-    `company` VARCHAR(255) NULL,
-    `companyAdress` VARCHAR(255) NULL,
-    `id_user` BIGINT NOT NULL,
-    `isValidated` BOOLEAN NOT NULL
-);
-ALTER TABLE
-    `sales` ADD CONSTRAINT `sales_id_buyer_foreign` FOREIGN KEY(`id_buyer`) REFERENCES `user`(`id`);
-ALTER TABLE
-    `book` ADD CONSTRAINT `book_id_image_foreign` FOREIGN KEY(`id_image`) REFERENCES `images`(`id`);
-ALTER TABLE
-    `book_categorie` ADD CONSTRAINT `book_categorie_id_book_foreign` FOREIGN KEY(`id_book`) REFERENCES `book`(`id`);
-ALTER TABLE
-    `sales` ADD CONSTRAINT `sales_id_isbn_foreign` FOREIGN KEY(`id_isbn`) REFERENCES `book`(`isbn`);
-ALTER TABLE
-    `book` ADD CONSTRAINT `book_id_author_foreign` FOREIGN KEY(`id_author`) REFERENCES `author`(`id`);
-ALTER TABLE
-    `book` ADD CONSTRAINT `book_id_saler_foreign` FOREIGN KEY(`id_saler`) REFERENCES `user_pro`(`id`);
-ALTER TABLE
-    `book_categorie` ADD CONSTRAINT `book_categorie_id_categorie_foreign` FOREIGN KEY(`id_categorie`) REFERENCES `categorie`(`id`);
-ALTER TABLE
-    `note` ADD CONSTRAINT `note_id_book_foreign` FOREIGN KEY(`id_book`) REFERENCES `book`(`id`);
-ALTER TABLE
-    `user_pro` ADD CONSTRAINT `user_pro_id_user_foreign` FOREIGN KEY(`id_user`) REFERENCES `user`(`id`);
-ALTER TABLE
-    `note` ADD CONSTRAINT `note_id_user_foreign` FOREIGN KEY(`id_user`) REFERENCES `user`(`id`);
-ALTER TABLE
-    `sales` ADD CONSTRAINT `sales_id_saler_foreign` FOREIGN KEY(`id_saler`) REFERENCES `user_pro`(`id`);
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : ven. 24 jan. 2025 à 15:41
+-- Version du serveur : 8.3.0
+-- Version de PHP : 8.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `bookmaker_td`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `mail` varchar(191) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `id_user_pro` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_mail_unique` (`mail`)
+) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `mail`, `password`, `lastname`, `firstname`, `role`, `id_user_pro`) VALUES
+(4, 'benny@gmail.com', '$2y$10$XqFp8vSr/e0n.iRUJ1RAO.44T5vanuOr6zdB6R/B3myjTNOUj33hS', 'Gagnaire', 'Benjamin', '', 0),
+(2, 'esteranodin@hotmail.fr', '$2y$10$SF.p.1O9gyQHKoPFYxDdDOAdEas.gAgOsr9gV.uMKY7LxUDFxSeG2', 'Sabatier', 'Marion', 'admin', 0),
+(6, 'laloutre@gmail.com', '$2y$10$LnhiPj8SN2yU0eQ2MjSZI.ifkH6vaV1ynvKt7k95dZIySWrhFQHhC', 'Carlier', 'Clément', '', 0),
+(9, 'supertemoin@gmail.com', '$2y$10$ZIsZN3p8EI6m8yTpMIYnA.PPrbQWRvli6j.QM.74.neMwIRvWGByC', 'Breisse', 'Stéphane', '', 0),
+(15, 'june@gmail.com', '$2y$10$yRemmSEt1vZwBXyIiRckzeMD.crGmFy6Mm30F5V2MpJeSqBXu3YrG', 'june', 'june', '', NULL),
+(11, 'lacdescygnes@gmail.com', '$2y$10$M4xcL73MyT3KwXN9X76mt.p3Uj5NXWYltxCe0x7RBDIeuV0zdOBDm', 'Kerever', 'Laouïg', '', 0),
+(12, 'potichat@gmail.com', '$2y$10$Y8SYCIsbWSSHC1RtpMCQBef1HB0pTJ.XRK5flSbpmYLufhfYGgKXa', 'Ponson', 'Adrien', '', 0);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
